4a8c6ce772c96f28bf1f52187c044681316c2a3a
Cheolkie Kim <ckiekim@naver.com>
2024-03-11 09:48:17 +0900

    Day 3

--- src/main/java/com/example/abbs/controller/TestController.java
+++ src/main/java/com/example/abbs/controller/TestController.java
@@ -1,15 +1,26 @@
 package com.example.abbs.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@PropertySource("classpath:static/data/myKeys.properties")
 @Controller
 public class TestController {
-
+	@Value("${key}") private String value;
+	@Value("${server.port}") private int port;
+	
 	@GetMapping("/index")
 	public String index() {
 		return "index";
 	}
 
+	@ResponseBody
+	@GetMapping("/value") 
+	public String value() {
+		return "value=" + value + ", port=" + port;
+	}
 	
 }
--- src/main/java/com/example/abbs/util/AsideUtil.java
+++ src/main/java/com/example/abbs/util/AsideUtil.java
@@ -3,10 +3,16 @@ package com.example.abbs.util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
+@PropertySource("classpath:static/data/myKeys.properties")
 public class AsideUtil {
+	@Value("roadAddrKey") private String roadAddrKey;
+	@Value("kakaoApiKey") private String kakaoApiKey;
+	@Value("openWeatherApiKey") private String openWeatherApiKey;
 
 	public String getTodayQuote(String filename) {
 		String result = null;
